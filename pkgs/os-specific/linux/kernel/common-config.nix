1d319f0b1d82b059a826729b6a517591ba6e1472
Ryan Burns <rtburns@protonmail.com>
2021-01-10 20:36:56 -0800

    kconfig: fix config on kernels < 5.4

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -419,6 +419,7 @@ let
       MODULE_SIG            = no; # r13y, generates a random key during build and bakes it in
       # Depends on MODULE_SIG and only really helps when you sign your modules
       # and enforce signatures which we don't do by default.
+    } // optionalAttrs (versionAtLeast version "5.4") {
       SECURITY_LOCKDOWN_LSM = no;
     } // optionalAttrs (!stdenv.hostPlatform.isAarch32) {
 
