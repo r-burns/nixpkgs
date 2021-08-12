d3ce5446115ea224f36ce2c66612ea01471e685a
Ryan Burns <rtburns@protonmail.com>
2021-08-12 12:31:27 -0700

    mesa: specify drivers for ppc64le (WIP)

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -5,9 +5,9 @@
 , llvmPackages, libffi, libomxil-bellagio, libva-minimal
 , libelf, libvdpau
 , libglvnd, libunwind
-, galliumDrivers ? ["auto"]
-, driDrivers ? ["auto"]
-, vulkanDrivers ? ["auto"]
+, galliumDrivers ? if stdenv.hostPlatform.isPower then ["nouveau" "swrast"] else ["auto"]
+, driDrivers ? if stdenv.hostPlatform.isPower then ["nouveau"] else ["auto"]
+, vulkanDrivers ? if stdenv.hostPlatform.isPower then [] else ["auto"]
 , eglPlatforms ? [ "x11" ] ++ lib.optionals stdenv.isLinux [ "wayland" ]
 , OpenGL, Xplugin
 , withValgrind ? !stdenv.isDarwin && lib.meta.availableOn stdenv.hostPlatform valgrind-light, valgrind-light
